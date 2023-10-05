cba98373315dc8f030070235d0e588d03b42aa6b
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-05 18:45:39 -0400

    Create Diamonds.py

---
+++ Diamonds.py
@@ -0,0 +1,9 @@
+"""
+Project 16, Diamonds: An algorithm for drawing diamonds of various sizes.
+Practice your pattern recognition skills to create drawing algorithms.
+
+This program features a small algorithm for drawing ASCII-art diamonds of various sizes. It
+contains functions for drawing either an outline or filled-in-style diamond of the size you
+dictate. These functions are good practice for a beginner; try to understand the pattern behind
+the diamond drawings as they increase in size.
+"""
