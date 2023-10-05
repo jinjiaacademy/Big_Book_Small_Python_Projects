cf50cad4988d6c1b7306760102eaff35d9b3d9e3
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-05 19:47:09 -0400

    Create DiceRoller.py

---
+++ DiceRoller.py
@@ -0,0 +1,12 @@
+"""
+Project 18, Dice Roller: A tool for reading Dungeons & Dragons dice notation to generate
+random numbers.
+Parse text to identify key strings.
+
+Dungeons & Dragons and other tabletop role-playing games use special dice that can have 4, 8,
+10, 12, or even 20 sides. These games also have a specific notation for indicating which dice to
+roll. For example, 3d6 means rolling three six-sided dice, while 1d10+2 means rolling one ten-sided
+die and adding a two-point bonus to the roll. This program simulates this dice rolling, in case you
+forgot to bring your own. It can also simulate rolling dice that don't physically exist, such as
+a 38-sided die.
+"""
