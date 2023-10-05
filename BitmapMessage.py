bceb3adfa29428a2c184273edf91ab827248f054
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-05 13:54:40 -0400

    Update BitmapMessage.py

--- BitmapMessage.py
+++ BitmapMessage.py
@@ -1,4 +1,12 @@
 """
 Project 3, Bitmap Message: Display a message on the screen configured by a 2D bitmap image.
 Work with multiline strings.
+
+This program uses a multiline string as a bitmap, a 2D image with only two possible colors
+for each pixel, to determine how it should display a message from the user. In this bitmap,
+space characters represent an empty space, and all other characters are replaced by characters
+in the suer's message. The provided bitmap resembles a world map, but you can change this to
+any image you'd like. The binary simplicity of the space-or-message-characters system makes
+it good for beginners. Try experimenting with different messages to see what the results look
+like.
 """
