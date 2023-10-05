ed70eb0ba5580c2f5684fd64b1c1ae1c3d91c799
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-05 16:50:23 -0400

    Create CaesarHacker.py

---
+++ CaesarHacker.py
@@ -0,0 +1,9 @@
+"""
+Project 7, Caesar Hacker: A program to decrypt Caesar cipher messages without the encryption key.
+Implement a brute-force cryptanalysis algorithm.
+
+This program can hack messages encrypted with the Caesar cipher from Project 6, even if you don't
+know the key. There are only 26 possible keys for the Caesar cipher, so a computer can easily try
+all possible decryptions and display the results to the user. Incryptography, we call this technique
+a brute-force attack.
+"""
