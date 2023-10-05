a7679264a8c4d4d14a388d735b6ad7e51428d2d5
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-05 17:06:25 -0400

    Create Cho-han.py

---
+++ Cho-han.py
@@ -0,0 +1,10 @@
+"""
+Project 10: Cho-han: A gambling dice game from feudal Japan.
+Practice using random numbers and dictionary data structures.
+
+Cho-han is a dice game played in gambling houses of feudal Japan. Two six-sided dice are rolled
+in a cup, and gamblers must guess if the sum is even (cho) or odd (han). The house takes a small
+cut of all winnings. The simple random number generation and basic math used to determine odd or
+even sums make this project especially suitable for beginners. More information about Cho-han can
+be found at: https://en.wikipedia.org/wiki/Ch%C5%8D-han.
+"""
