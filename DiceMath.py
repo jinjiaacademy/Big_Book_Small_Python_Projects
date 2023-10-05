17e590221a874c156fd3644874b438b87db263e2
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-05 19:13:06 -0400

    Create DiceMath.py

---
+++ DiceMath.py
@@ -0,0 +1,9 @@
+"""
+Project 17, Dice Math: A visual dice-rolling math game.
+Use dictionary data structures for screen coordinates.
+
+This math quiz program rolls two to six dice whose sides you must add up as quickly as
+possible. But this program operates as more than just automated flash cards; it draws
+the faces of the dice to random places on the screen. The ASCII-art aspect adds a fun
+twist while you practice arithmetic.
+"""
