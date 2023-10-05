2fe902d87f34d5d58809b15749fdc072c2525861
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-05 19:51:46 -0400

    Create Ducklings.py

---
+++ Ducklings.py
@@ -0,0 +1,9 @@
+"""
+Project 22, Ducklings: Mix and match strings to create a variety of ASCII-art ducks.
+Use object-oriented programming to create a data model for duck drawings.
+
+This program creates a scrolling field of ducklings. Each duckling has slight variations:
+they can face left or right and have two different body sizes, four types of eyes, two types
+of mouths, and three positions for their wings. This gives us 96 different possible variations,
+which the Ducklings program produces endlessly.
+"""
