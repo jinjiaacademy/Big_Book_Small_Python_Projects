6c3722d43fa3c284679dc90237a69eb66b1a043e
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-05 17:06:19 -0400

    Create CarrotInABox.py

---
+++ CarrotInABox.py
@@ -0,0 +1,12 @@
+"""
+Project 9, Carrot in a Box: A silly bluffing game between two players.
+Create ASCII art.
+
+This is a simple and silly bluffing game for two human players. Each player has a box. One box
+has a carrot in it, and each player wants to have the carrot. The first player looks in their
+box and then tells the second player they either do or don't have the carrot. The second player
+gets to decide whether to swap boxes or not.
+The ASCII art in the code makes typing this program take a while (though copying and pasting the
+ASCII art can speed up the task), but this project is good for beginners because it is straight-
+forward, with minimal looping and no defined functions.
+"""
