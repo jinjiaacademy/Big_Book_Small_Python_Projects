bfc5d00985b70a24897f9fd622ee0da68110c075
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-05 18:45:24 -0400

    Create Countdown.py

---
+++ Countdown.py
@@ -0,0 +1,10 @@
+"""
+Project 14, Countdown: A countdown timer with a seven-segment display.
+Practice importing modules you create.
+
+This program displays a digital timer that counts down to zero. Rather than render numeric
+characters directly, the sevseg.py module from Project 64, "Seven-Segment Display Module",
+generates the drawings for each digit. You must create this file before the Countdown program
+can work. Then, set the countdown timer to any number of seconds, minutes, and hours you like.
+This program is similar to Project 19, "Digital Clock."
+"""
