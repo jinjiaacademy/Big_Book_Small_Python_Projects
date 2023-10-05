f48b5a7927bee3c9ede7c7dc83e020ce12873d9c
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-05 14:07:19 -0400

    Create BouncingDVDLogo.py

---
+++ BouncingDVDLogo.py
@@ -0,0 +1,16 @@
+"""
+Simulates the colorful bouncing DVD logo of decades past.
+Work with coordinates and colorful text.
+
+If you are of a certain age, you'll remember those ancient technological devices called
+DVD players. When not playing DVDs, they would display a diagonally traveling DVD logo
+that bounced off the edges of the screen. This program simulate this colorful DVD logo
+by making it change direction each time it hits an edge. We'll also keep track of how many
+times a logo hits a corner of the screen. This creates an interesting visual animation to
+look at, especially for the magical moment when a logo lines up perfectly with a corner.
+
+You can't run this program from your integrated development environment (IDE) or editor
+because it uses the bext module. Therefore, it must be run from the command prompt or Terminal
+in order to display correctly. You can find more information about the bext module at
+https://pypi.org/project/bext/.
+"""
