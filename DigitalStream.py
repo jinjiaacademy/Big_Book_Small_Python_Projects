3d1c2ad1534c2251b4700dffc6085f70bff22f07
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-05 19:47:18 -0400

    Create DigitalStream.py

---
+++ DigitalStream.py
@@ -0,0 +1,10 @@
+"""
+Project 20: Digital Stream: a scrolling screensaver that resembles The Matrix.
+Experiment with different animation speeds.
+
+This program minics the "digital stream" visualization from the science fiction movie
+The Matrix. Random beads of binary "rain" stream up from the bottom of the screen, creating
+a cool, hacker-like visualization. (Unfortunately, due to the way text moves as the screen
+scrolls down, it's not possible to make the streams fall downward without using a module such
+as bext.)
+"""
