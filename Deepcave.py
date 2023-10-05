f9d9cacfb3c5e46f009ba561b5f0f84795283b52
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-05 18:45:34 -0400

    Create Deepcave.py

---
+++ Deepcave.py
@@ -0,0 +1,9 @@
+"""
+Project 15, Deep Cave: A tunnel animation that descends endlessly into the earth.
+Use string replication and simple math.
+
+This program is an animation of a deep cave that descends forever into the earth. Although
+short, this program takes advantage of the scrolling nature of the computer screen to produce
+an interesting and unending visualization, proof that it doesn't take much code to produce
+something fun to watch. This program is similar to Project 58, "Rainbow."
+"""
