b85cf1b5dea15225106411ab978b77e74d4728dd
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-05 16:55:19 -0400

    Create CalendarMaker.py

---
+++ CalendarMaker.py
@@ -0,0 +1,10 @@
+"""
+Project 8: Calendar Maker: Create calendar pages for a given year and month.
+Use Python's datetime module and the timedelta data type.
+
+This program generates priintable text files of monthly calendars for the month and year you enter.
+Dates and calendars are a tricky topic in programming because there are so many different rules for
+determining the number of days in a month, which years are leap years, and which day of the week a
+particular date falls on. Fortunately, Python's datetime module handles these details for you. This
+program focuses on generating the multiline string for the monthly calendar page.
+"""
