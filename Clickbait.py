0ecdfd97bd41228fdb85a79a3e42c3b451558c4c
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-05 18:09:36 -0400

    Create Clickbait.py

---
+++ Clickbait.py
@@ -0,0 +1,11 @@
+"""
+Project 11, Clickbait Headline Generator: A humorous headline generator for your content farm.
+Practice string manipulation and text generation.
+
+Our website needs to trick people into looking at advertisements! But coming up with creative,
+original content is too hard. Luckily, with the clickbait headline generator, we can make a computer
+come up with millions of outrageous fake headlines. They're all low quality, but readers don't seem
+to mind. This program generates as many headlines as you need from a Mad Libs-style template.
+There's a lot of text in this program for the headline templates, but the code itself is straight-
+forward and suitable for beginners.
+"""
