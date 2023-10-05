035db94ab8188d4cbd8df33b8cd95f979823cac1
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-05 19:47:24 -0400

    Create DNA.py

---
+++ DNA.py
@@ -0,0 +1,12 @@
+"""
+Project 21, DNA Visualization: An endless ASCII-art double helix that demonstrates the structure
+of DNA.
+Work with string templates and randomly generated text.
+
+Deoxyribonucleic acid is a tiny molecule that exists in every cell of our bodies and contains
+the blueprint for how our bodies grow. It looks like a double helix (a sort of twisted ladder)
+of pairs of nucleotide molecules: guanine, cytosine, adenine, and thymine. These are represented
+by the letters G, C, A, and T. DNA is a long molecule; it's microscopic, but if it were stretched
+out, its 3 billion base pairs would be 2 meters long. This program is a simple animation of DNA.
+
+"""
