c568841dba780056fe12514df94dadaccb2f5f3e
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-05 18:27:35 -0400

    Create Collatz.py

---
+++ Collatz.py
@@ -0,0 +1,14 @@
+"""
+Project 12, Collatz Sequence: Explore the simplest impossible conjecture in mathematics.
+Learn about the modulus operator.
+
+The Collatz sequence, also called the 3n+1 problem, is the simplest impossible math problem.
+(But don't worry, the program itself is easy enough for beginners.) From a starting number, n,
+follow three rules to get the next number in the sequence:
+    1. If n is even, the next number n is n/2.
+    2. If n is odd, the next number n is n*3+1.
+    3. If n is 1, stop. Otherwise, repeat.
+It is generally thought, but so far not mathematically proven, that every starting number
+eventually terminates at 1. More information about the Collatz sequence can be found at
+https://en.wikipedia.org/wiki/Collatz_conjecture
+"""
