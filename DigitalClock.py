2a1350a49e8f5f315724a5a80b9b419cfd813484
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-05 19:47:14 -0400

    Create DigitalClock.py

---
+++ DigitalClock.py
@@ -0,0 +1,8 @@
+"""
+Project 19, Digital Clock: A clock with a calculator-like display.
+Generate numbers that match information from the datetime module.
+
+This program displays a digital clock with the current time. Rather than render numeric characters
+directly, the sevseg.py module from Project 64, "Seven-Segment Display Module," generates the
+drawings for each digit. This program is similar to Project 14, "Countdown".
+"""
