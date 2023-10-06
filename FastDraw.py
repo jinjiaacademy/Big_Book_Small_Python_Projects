1e05f4b4da0af084d3927f8249e3630e51f99c44
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-05 21:48:26 -0400

    Create FastDraw.py

---
+++ FastDraw.py
@@ -0,0 +1,8 @@
+"""
+Project 25, Fast Draw: Test your reflexes to see if you're the fastest keyboard in the West.
+Learn about the keyboard buffer.
+
+This program tests your reaction speed: Press ENTER as soon as you see the word DRAW.
+But careful, though. Press it before DRAW appears, and you lose. Are you the fastest
+keyboard in the west?
+"""
