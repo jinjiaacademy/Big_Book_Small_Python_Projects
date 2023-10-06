e179c8e2d71a107610dbf578f24a4a583ec2150b
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-05 21:48:37 -0400

    Create FishTank.py

---
+++ FishTank.py
@@ -0,0 +1,12 @@
+"""
+Project 27, Fish Tank: A colorful, animated ASCII-art fish tank.
+Use screen coordinates, text colors, and data structures.
+
+Watch your own virtual fish in a virtual fish tank, complete with air bubblers and
+kelp plants. Each time you run the program, it randomly generates the fish using different
+fish types and colors. Take a break and enjoy the calm serenity of this software aquarium,
+or try programming in some virtual sharks to terrorize its inhabitants. You can't run this
+program from your IDE or editor. This program uses the bext module and must be run from the
+Command Prompt or Terminal in order to display correctly. More information about the bext
+module can be found at https://pypi.org/project/Bext/
+"""
