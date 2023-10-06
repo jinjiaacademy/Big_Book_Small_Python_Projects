415282c9699faedf36f71119cd796035fc851f98
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-06 13:09:52 -0400

    Create NinetyNineBottles.py

---
+++ NinetyNineBottles.py
@@ -0,0 +1,15 @@
+"""
+Project 50, Ninety-Nine Bottles: Display the lyrics to a repetitive song.
+Use loops and string templates to produce text.
+
+“Ninety-Nine Bottles” is a folk song of undetermined origin known for its length and repetitiveness.
+The lyrics go,
+“Ninetynine bottles of milk on the wall, ninety-nine bottles of milk.
+Take one down, pass it around, ninety eight bottles of milk on the wall.”
+As the lyrics repeat, the number of bottles falls from ninety-eight to ninetyseven, then from
+ninety-seven to ninety-six, until it reaches zero: “One bottle of milk on the wall, one bottle
+of milk. Take it down, pass it around, no more bottles of milk on the wall!”
+Luckily for us, computers are excellent at performing repetitive tasks, and this program reproduces
+all of the lyrics programmatically. An extended version of this program is in Project 51,
+“niNety-nniinE BoOttels.”
+"""
