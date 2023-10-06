e0a0ecb8b27e69af87f6e9299aefef9f3867023a
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-05 20:34:56 -0400

    Create EtchingDrawer.py

---
+++ EtchingDrawer.py
@@ -0,0 +1,11 @@
+"""
+Project 23, Etching Drawer: Move the cursor to create line drawings.
+Work with screen coordinates and relative directional movements.
+
+When you move a pen point around the screen with the WASD keys, the etching drawer forms
+a picture by tracing a continuous line, like the Etch A Sketch toy. Let your artistic side
+break out and see what images you can create. This program also lets you save your drawings
+to a text file so you can print them out later. Plus, you can copy and paste the WASD movements
+of other drawings into this program, like the WASD commands for the Hilbert Curve fractal
+presented on line 6 to 14 of the source code.
+"""
