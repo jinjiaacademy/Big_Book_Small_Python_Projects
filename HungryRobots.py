df4efc3f5101711aba08e001bd79a6dc5cae615f
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-06 10:36:25 -0400

    Create HungryRobots.py

---
+++ HungryRobots.py
@@ -0,0 +1,11 @@
+"""
+Project 37, Hungry Robots: Avoid killer robots in a maze.
+Create a simple AI for robot movements.
+
+You are trapped in a maze with hungry robots. You don't know why robots need to eat,
+but you don't want to find out. The robots are badly programmed and will move directly
+toward you, even if blocked by walls. You must trick the robots into crashing into each
+other (or dead robots) without being caught.
+You have a personal teleporter device that can send you to a random new place, but it only
+has enough battery for two trips. Also, you and the robots can slip through corners.
+"""
