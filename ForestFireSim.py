e989883fa29b217b4191f2b618f470f37d1e8a42
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-05 22:34:03 -0400

    Create ForestFireSim.py

---
+++ ForestFireSim.py
@@ -0,0 +1,11 @@
+"""
+Project 29, Forest Fire Sim: Simulate the spread of wildfires through a forest.
+Create a simulation with adjustable parameters.
+
+This simulation shows a forest whose trees are constantly growing and then being burned down.
+On each step of the simulation, there is a 1 percent chance that a blank space grows into a
+tree adn a 1 percent chance that a tree is struck by lightning and burns. Fires will spread
+to adjacent trees, so a densely packed forest is more likely to suffer a larger fire than a
+sparsely packed one. This simulation was inspired by Nicky Case's Emoji Sim at
+https://ncase.me/simulating/model/
+"""
