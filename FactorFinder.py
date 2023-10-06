0802b3014d91f0f7ae169c12445dc5b10f22e6af
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-05 20:35:01 -0400

    Create FactorFinder.py

---
+++ FactorFinder.py
@@ -0,0 +1,13 @@
+"""
+Project 24: Factor Finder: Find all the multiplicative factors of a number.
+Use the modulus operator and Python's math module.
+
+A number's factors are any two other numbers that, when multiplied with each other, produce
+the number. For example, 2 x 13 = 26, so 2 and 13 are factors of 26. Also, 1 x 26 = 26, so 1
+and 26 are also factors of 26. Therefore, we say that 26 has four factors: 1, 2, 13, and 26.
+If a number only has two factors (1 and itself), we call that a prime number. Otherwise, we call
+it a composite number. Use the factor finder to discover some new prime numbers. (Hint: Prime
+numbers always end with an odd number that isn't 5.) You can also have the computer calculate
+them with Project 56, "Prime Numbers."
+The math for this program isn't too heavy, making it an ideal project for beginners.
+"""
