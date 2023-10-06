2f6f57ec887a36965df5d86d31d3451970554d69
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-06 14:02:13 -0400

    Create PrimeNumbers.py

---
+++ PrimeNumbers.py
@@ -0,0 +1,12 @@
+"""
+Project 56, Prime Numbers: Calculate prime numbers.
+Learn math concepts and use Python's math module.
+
+A prime number is a number that is evenly divisible only by one and itself. Prime numbers have
+a variety of practical applications, but no algorithm can predict them; we must calculate them
+one at a time. However, we do know that there is an infinite number of prime numbers to be discovered.
+This program finds prime numbers through brute-force calculation. Its code is similar to Project 24,
+"Factor Finder." (Another way to describe a prime number is that one and the number itself are its
+only factors.) You  can find out more about prime numbers from
+https://en.wikipedia.org/wiki/Prime_number
+"""
