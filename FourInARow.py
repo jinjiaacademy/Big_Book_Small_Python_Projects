6a24a29e5fd29ded41c220cfadec06bc98ff74b7
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-06 09:49:02 -0400

    Create FourInARow.py

---
+++ FourInARow.py
@@ -0,0 +1,8 @@
+"""
+Project 30, Four in a row: A board game where two players try to connect four tiles in a row.
+Create a data structure that mimics grevity.
+
+In this classic tile-dropping board game for two players, you must try to get four of your
+tiles in a row horizontally, vertically, or diagonally, while preventing your opponent from
+doing the same. This program is similar to Connect Four.
+"""
