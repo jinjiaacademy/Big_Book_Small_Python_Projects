abab41acb924f17f9e11b2f7e2b2d1e19f9f7f11
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-06 12:53:38 -0400

    Create MondrianArtGenerator.py

---
+++ MondrianArtGenerator.py
@@ -0,0 +1,11 @@
+"""
+Project 47, Mondrian Art Generator: Create geometric drawings in the style of Piet Mondrian
+Implement an art-generating algorithm.
+
+Piet Mondrian was a 20th-century Dutch painter and one of the founders of neoplasticism, an
+abstract art movement. His most iconic paintings relied on blocks of primary colors (blue,
+yellow, red), black, and white. Using a minimalist approach, he separated these colors with
+horizontal and vertical elements.
+This program generates random paintings that follow Mondrian's style. You can find out more
+about Piet Mondrian at https://en.wikipedia.org/wiki/Piet_Mondrian
+"""
