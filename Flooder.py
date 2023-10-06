5cdaead28529d6c4e8470d71e99798c46df7a52b
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-05 21:48:42 -0400

    Create Flooder.py

---
+++ Flooder.py
@@ -0,0 +1,11 @@
+"""
+Project 28, Flooder: Attempt to fill the entire puzzle board with one color.
+Implement the flood fill algorithm.
+
+Flooder is a colorful game where a player tries to fill the board with a single color
+by changing the color of the tile in the upper-left corner. This new color spreads to
+all neighboring tiles that matched the original color. It's similar to the Flood It
+mobile game. This program also has a  colorblind mode, which uses shapes instead of flat
+colored tiles. It relies on the recursive flood fill algorithm to paint the board and
+works similarly to the "paint bucket" or "fill" tool in many painting applications.
+"""
