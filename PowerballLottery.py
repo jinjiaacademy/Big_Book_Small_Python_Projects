64149ec335cbd5fdda3535161bec07c082101a99
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-06 14:02:07 -0400

    Create PowerballLottery.py

---
+++ PowerballLottery.py
@@ -0,0 +1,17 @@
+"""
+Project 55, Powerball Lottery: Simulate losing at the lottery thousands of times.
+Explore probability using random numbers.
+
+The Powerball Lottery is an exciting way to lose small amounts of money. If you purchase a $2 ticket,
+you can pick six numbers: five drawn from 1 to 69, and a sixth "Powerball" number drawn from 1 to 26.
+The order of the numbers doesn't matter. If the lottery selects your six numbers, you win $1.586 billion
+dollars. Except you won't win, because your odds are 1 in 292,201,338. But if you spent $200 on 100
+tickets, your odds would be ... 1 in 2,922,013. You won't win that either, but at least you'll lose 100
+times as much money. The more you like losing money, the more fun the lottery is!
+To help you visualize how often you won't win the lottery, this program simulates up to one million
+Powerball drawings and then compares them with the numbers you picked. Now you can have all the excite-
+ment of losing the lottery without spending money.
+Fun fact: every set of six numbers is just as likely to win as any other. So the next time you want
+to buy a lottery ticket, pick the numbers 1, 2, 3, 4, 5, and 6. Those numbers are just as likely to
+come up as a more complex set.
+"""
