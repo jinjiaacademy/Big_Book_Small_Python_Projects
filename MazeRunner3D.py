ca2cd094093343e61f2d7b8b72d5290a944c8694
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-06 11:48:18 -0400

    Create MazeRunner3D.py

---
+++ MazeRunner3D.py
@@ -0,0 +1,9 @@
+"""
+Project 45, Maze Runner 3D: Try to escape a maze ... in 3D!
+Modify multiline strings to display a 3D view.
+
+This three-dimensional maze runner provides the player with a first-person view from
+inside a maze. Try to find your way out! You can generate maze files by following the
+instructions in Project 44, "Maze Runner 2D," or by downloading maze files from
+https://invpy.com/mazes/
+"""
