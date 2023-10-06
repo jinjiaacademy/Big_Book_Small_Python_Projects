85b601f270febb340d5f62b8706ee9e3b8eaf6d0
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-06 13:09:56 -0400

    Create NinetyNineBottles2.py

---
+++ NinetyNineBottles2.py
@@ -0,0 +1,11 @@
+"""
+Project 51, niNety-nniinE BoOttels: Display the lyrics to a repetitive song that get more distorted
+with each verse.
+Manipulate strings to introduce distortions.
+
+In this version of the song "Ninety-Nine Bottles", the program introduces small imperfections in
+each stanza by either removing a letter, swapping the casing of a letter, transposing two letters,
+or doubling a letter.
+As the song continues to play, these mutations add up, resulting in a very silly song. It's a good
+idea to try Project 50, "Ninety-Nine Bottles," before attempting this one.
+"""
