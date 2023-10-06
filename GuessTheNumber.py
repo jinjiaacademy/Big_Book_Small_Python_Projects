107e73baf9bf9a6b9c2cfcf93927a302fb7b383c
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-06 10:01:03 -0400

    Create GuessTheNumber.py

---
+++ GuessTheNumber.py
@@ -0,0 +1,9 @@
+"""
+Project 31, Guess the Number: The classic number guessing game.
+Program basic concepts for beginners.
+
+Guess the Number is a classic game for beginners to practice basic programming techniques.
+In this game, the computer thinks of a random number between 1 and 100. The palyer has 10 chances
+to guess the number. After each guess, the computer tells the player if it was too high or
+too low.
+"""
