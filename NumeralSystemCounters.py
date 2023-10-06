5c5d0ef61528e94d559c171e5d7c1b72fca412d2
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-06 13:32:16 -0400

    Create NumeralSystemCounters.py

---
+++ NumeralSystemCounters.py
@@ -0,0 +1,12 @@
+"""
+Project 52, Numeral Systems Counters: Examine binary and hexadecimal numbers.
+Use Python's number conversion functions.
+
+We're used to counting in the decimal numeral system, which uses 10 digits: 0 through 9. This system
+likely developed because humans counted on their fingers, and most people have 10 fingers. But other
+number systems exist. Computers make use of binary, a numeral system with only two digits, 0 and 1.
+Programmers also sometimes use hexadecimal, which is a base-16 numeral system that uses the digits
+0 to 9 but also extends into the letters A to F.
+We can represent any number in any numeral system, and this program displays a range of numbers in
+decimal, binary, and hexadecimal.
+"""
