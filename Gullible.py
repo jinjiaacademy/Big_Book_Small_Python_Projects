9fea5bf76284d5511b51094fb67fb8e0181624ba
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-06 10:01:07 -0400

    Create Gullible.py

---
+++ Gullible.py
@@ -0,0 +1,9 @@
+"""
+Project 32, Gullible: A humorous program to keep gullible people busy for hours.
+Use input validation and loops.
+
+In this short and simple program, you can learn the secret and subtle art of keeping
+a gullible person busy for hours. I won't spoil the punch line here. Copy the code and
+run it for yourself. This project is great for beginners, whether you're smart or ...
+not so smart.
+"""
