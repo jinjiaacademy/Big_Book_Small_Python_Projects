fa2d0b94b398a0dcf92494a995b09bba5ede3c36
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-06 10:36:05 -0400

    Create HangmanAndGuillotine.py

---
+++ HangmanAndGuillotine.py
@@ -0,0 +1,13 @@
+"""
+Project 34, Hangman and Guillotine: The classic word guessing game.
+Use string manipulation and ASCII art.
+
+This classic word game has the player guess the letters to a secret word. For each
+incorrect letter, another part of the hangman is drawn. Try to guess the complete
+word before the hangman completes. The secret words in this version are all animals
+like RABBIT and PIGEON, but you can replace these with your own set of words.
+The HANGMAN_PICS variable contains ASCII-art strings of each step of the hangman's
+noose.
+For a French twist on the game, you can replace the strings in the HANGMAN_PICS variable
+with the following strings depicting a guillotine.
+"""
