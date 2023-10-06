553b1edef904025800a997792586c76f1f424d03
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-05 21:48:32 -0400

    Create Fibonacci.py

---
+++ Fibonacci.py
@@ -0,0 +1,14 @@
+"""
+Project 26: Finonacci: Generate numbers in the famous Fibonacci sequence.
+Implement a rudimentary mathematics algorithm.
+
+The fibonacci sequence is a famous mathematical pattern credited to Italian mathematician
+Fibonacci in the 13th century (though others had discovered it even earlier). The sequence
+begins with 0 and 1, and the next number is always the sum of the previous two numbers.
+The sequence continues forever:
+    0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ......
+The Fibonacci sequence has applications in music composition, stock market prediction, the
+pattern of florets in the head of sumflowers, and many other areas. This program lets you
+calculate the sequence as high as you are willing to go. More information about the Fibonacci
+sequence can be found at https://en.wikipedia.org/wiki/Fibonacci_sequence
+"""
