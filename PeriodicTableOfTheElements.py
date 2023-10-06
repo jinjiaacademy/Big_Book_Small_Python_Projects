dadacd0bd5aadc49dfde15ada012a95d7356da05
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-06 13:41:30 -0400

    Create PeriodicTableOfTheElements.py

---
+++ PeriodicTableOfTheElements.py
@@ -0,0 +1,10 @@
+"""
+Project 53, Periodic Table of the Elements: An interactive database of chemical elements.
+Parse CSV files to load data into a program.
+
+The periodic table of the elements organizes all known chemical elements into a single table.
+This program presents this table and lets the player access additional information about each
+element, such as its atomic number, symbol, melting point, and so on. I compiled this information
+from Wikipedia and stored it in a file called periodictable.csv that you can download from
+https://inventwithpython.com/periodictable.csv.
+"""
