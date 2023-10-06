20dc178dfeb07d7b8da480004b457ea6d59b4413
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-06 10:36:18 -0400

    Create Hourglass.py

---
+++ Hourglass.py
@@ -0,0 +1,8 @@
+"""
+Project 36, Hourglass: A simple physics engine for falling sand.
+Simulate gravity and use collision detection.
+
+This visualization program has a rough physics engine that simulates sand falling through
+the small aperture of an hourglass. The sand piles up in the bottom half of the hourglass,
+then the hourglass is turned over so the process repeats.
+"""
