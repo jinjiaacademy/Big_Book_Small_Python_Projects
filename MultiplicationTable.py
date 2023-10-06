37327766752911d58bac0baff65e2a315a7bfa9e
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-06 12:57:14 -0400

    Create MultiplicationTable.py

---
+++ MultiplicationTable.py
@@ -0,0 +1,7 @@
+"""
+Project 49, Multiplication Table: Display the multiplication table up to 12 x 12.
+Practice spacing text.
+
+This program generates a multiplication table from 0 x 0 to 12 x 12. While simple, it provides
+a useful demonstration of nested loops.
+"""
