1abb85b9bec485d35dd3780f92325ac051c558f5
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-06 11:43:33 -0400

    Create Mancala.py

---
+++ Mancala.py
@@ -0,0 +1,17 @@
+"""
+Project 43, Mancala: The ancient two-player board game from Mesopotamia.
+Use ASCII art and string templates to draw a board game.
+
+The board game Mancala is at least 2000 years old, making it almost as old as Project 63,
+"Royal Game of Ur." It is a "seed-sowing" game in which two players select pockets of seeds
+to spread across the other pockets on the board while trying to collect as many in their
+store as possible. There are several variants of this game across different cultures. The
+name comes from the Arab word naqala, meaning "to move."
+To play, grab the seeds from a pit on your side of the board and place one in each subsequent
+pit, going counterclockwise and skipping your opponent's store. If your last seed lands in an
+empty pit of yours, move the opposite pit's seeds into that pit. If the last placed seed is in
+your store, you get a free turn.
+The game ends when all of one player's pits are empty. The other player claims the remaining
+seeds for their store, and the winner is the one with the most seeds. More information about
+Mancala and its variants can be found at https://en.wikipedia.org/wiki/Mancala
+"""
