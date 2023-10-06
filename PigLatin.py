e931522fa965ef0d32a5c45d12e1621adf5ca1f6
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-06 14:02:01 -0400

    Create PigLatin.py

---
+++ PigLatin.py
@@ -0,0 +1,10 @@
+"""
+Project 54, Pig Latin: Translates English messages into lgpay Atinlay.
+Use text parsing and string manipulation.
+
+Pig Latin is a word game that transforms English words into a parody of Latin. In Pig Latin,
+if a word begins with a consonant, the speaker removes this letter and puts it at the end, followed
+by "ay". For example, "pig" becomes "igpay" and "latin" becomes "atinlay". Otherwise, if the word
+begins with a vowel, the speaker simply adds "yay" to the end of it. For example, "elephant" becomes
+"elephantyay" and "umbrella" becomes "umbrellayay".
+"""
