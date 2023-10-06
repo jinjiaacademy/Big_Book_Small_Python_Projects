e49195a745f600cf4584857a5510f362d03366b9
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-06 11:23:12 -0400

    Create MagicFortuneBall.py

---
+++ MagicFortuneBall.py
@@ -0,0 +1,11 @@
+"""
+Project 42, Magic Fortune Ball: A program to answer your yes/no questions about the future.
+Add cosmetic features to make basic text appear more interesting.
+
+The Magic Fortune Ball can predict the future and answer your yes/no questions with 100 percent
+accuracy using the power of Python's random number module. This program is similar to a Magic 8
+Ball toy, except you don't have to shake it. It also features a function for slowly printing text
+strings with spaces in between each character, giving the messages a spooky, mysterious effect.
+Most of the code is dedicated to setting the eerie atmosphere. The program itself simply selects
+a message to display in response to a random number.
+"""
