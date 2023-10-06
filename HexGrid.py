0be5599c04a69fccd7521f730fa7562fd31b9ba5
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-06 10:36:10 -0400

    Create HexGrid.py

---
+++ HexGrid.py
@@ -0,0 +1,10 @@
+"""
+Project 35, Hex Grid: Progammatically generate tiled ASCII art.
+Use loops to make repeating text patterns.
+
+This short program produces a tessellated image of a hexagonal grid, similar to chicken wire.
+It shows that you don't need a lot of code to make something interesting. A slightly more
+complicated variation of this program is Project 65, "Shining Carpet."
+Note that this program uses raw strings, which prefix the opening quote with a lowercase r so
+that the backslashes in the string aren't interpreted as escape characters.
+"""
