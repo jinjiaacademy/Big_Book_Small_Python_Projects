807194f9736f9e178fcfc8bb3bd3852382ebc7b7
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-06 10:08:21 -0400

    Create HackingMinigame.py

---
+++ HackingMinigame.py
@@ -0,0 +1,12 @@
+"""
+Project 33, Hacking Minigame: Deduce a password based on clues.
+Add cosmetic features to make a basic game more interesting.
+
+In this game, the player must hack a computer by guessing a seven-letter word used as the
+secret password. The computer's memory banks display the possible words, and the player is
+given hints as to how close each guess was. For example, if the secret password is MONITOR
+but the player guessed CONTAIN, they are given the hint that two out of seven letters were
+correct, because both MONITOR and CONTAIN have the letter O and N as their second and third
+letter. This game is similar to Project 1, "Bagels", and the hacking minigame in the Fallout
+series of video games.
+"""
