607933d7d1b6bbe425d17de26b730870047bf589
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-06 14:06:32 -0400

    Create ProgressBar.py

---
+++ ProgressBar.py
@@ -0,0 +1,9 @@
+"""
+Project 57, Progress Bar: A sample progress bar animation to use in other programs.
+Use the backspace-printing technique to create animations.
+
+A progress bar is a visual element that shows how much of a task has been completed. Progress
+bars are often used alongside downloading files or software installations. This project creates
+a getProgressBar() function that returns a progress bar string based on the arguments passed to
+it. It simulates a downloading file, but you can reuse the progress bar code in your own projects.
+"""
