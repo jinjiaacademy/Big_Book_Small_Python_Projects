5ca93cd3878b18c4744dd98d0f1b9d185f3e5737
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-06 11:15:33 -0400

    Create Leetspeak.py

---
+++ Leetspeak.py
@@ -0,0 +1,14 @@
+"""
+Project 40, Leetspeak: Translate English messages into |33t5p34]<.
+Use text parsing and string manipulation.
+
+There's no better way to demonstrate your mad hacker skills than by replacing letters
+in your text with numbers: m4d h4x0r 5k1llz!!! This word program automatically converts
+plain English into leetspeak, the coolest way to talk online. Or at least it was in 1993.
+It takes a while to get used to, but with some practice, you'll eventually be able to read
+leetspeak fluently. For example, 1t +@]<3s 4 w|-|1le +o
+g37 |_|s3|) 70, b|_|+ y0u (an 3\/3nt|_|/-\lly r3a|) l33t$peak phl|_|3n+ly.
+Leetspeak may be hard to read at first, but the program itself is simple and good for beginners.
+More information about leetspeak can be found at
+https://en.wikipedia.org/wiki/Leet
+"""
