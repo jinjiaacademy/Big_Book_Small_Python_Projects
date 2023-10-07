698bf105a14a6e7a6a9c410c1931c97ca6691535
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-07 10:06:51 -0400

    Create SevenSegmentDisplayModule.py

---
+++ SevenSegmentDisplayModule.py
@@ -0,0 +1,12 @@
+"""
+Project 64, Seven-Segment Display Module: A display like those used in calculators and microwave ovens.
+Create modules for use in other programs.
+
+A seven-segment display is a type of LCD component used to display numbers in pocket calculators,
+microwave ovens, and other small electornic devices. Through different combinations of seven line-
+shaped segments in an LCD, a seven-segment display can represent the digits 0 through 9.
+The benefit of this program is that other programs can import it as a module. Project 14, "Countdown"
+and Project 19, "Digital Clock" import the sevseg.py file so they can use its getSevSegStr() function.
+You can find more information about seven-segment displays and other variations at
+https://en.wikipedia.org/wiki/Seven-segment_display
+"""
