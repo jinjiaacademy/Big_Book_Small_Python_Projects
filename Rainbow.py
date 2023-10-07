2392e209021755c73e2e091a0130075cd3f332b0
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-07 09:02:47 -0400

    Create Rainbow.py

---
+++ Rainbow.py
@@ -0,0 +1,9 @@
+"""
+Project 58, Rainbow: A simple rainbow animation.
+Create animation for beginners.
+
+Rainbow is a simple program that shows a colorful rainbow traveling back and forth across the
+screen. The program makes use of the fact that when new lines of text appear, the existing text
+scrolls up, causing it to look like it's moving. This program is good for beginners, and it's
+similar to Project 15, "Deep Cave."
+"""
