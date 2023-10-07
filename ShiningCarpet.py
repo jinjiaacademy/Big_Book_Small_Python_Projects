15883f8b948c2c4911961c6a423a44748c6d57c8
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-07 10:06:56 -0400

    Create ShiningCarpet.py

---
+++ ShiningCarpet.py
@@ -0,0 +1,12 @@
+"""
+Project 65, Shining Carpet: Programmatically generate the carpet from The Shining.
+Use loops to make repeating text patterns.
+
+The Shining, a 1980 psychological horror film directed by Stanley Kubrick, takes place at the
+haunted Overlook Hotel. The hotel carpet's hexagonal design bacame an iconic part of this famous
+movie. The carpet features alternating and interlocking hexagons whose mesmerizing effect is well-
+suited for such an unnerving film. The short program in this project, similar to Project 35, "Hex
+Grid" prints this repetitive pattern on the screen.
+Note that this program uses raw strings, which prefix the opening quote with a lowercase r, so that
+the backslashes in the string aren't interpereted as escape characters.
+"""
