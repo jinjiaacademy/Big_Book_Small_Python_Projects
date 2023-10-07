15136b48367db32fb3e3482a00cdd3e6a43721a7
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-07 11:36:57 -0400

    Create TicTacToe.py

---
+++ TicTacToe.py
@@ -0,0 +1,8 @@
+"""
+Project 76, Tic-Tac-Toe: The classic two-player board game of Xs and Os.
+Create a data structure and helper functions.
+
+Tic-tac-toe is a classic pencil-and-paper game played on a 3x3 grid. Players take turns placing
+their X or O marks, trying to get three in a row. Most games of tic-tac-toe end in a tie, but it
+is possible to outsmart your opponent if they're not careful.
+"""
