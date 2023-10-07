cc9636e90dd6dc4d7eed35aceef0e3c174219faa
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-07 11:37:02 -0400

    Create TowerOfHanoi.py

---
+++ TowerOfHanoi.py
@@ -0,0 +1,10 @@
+"""
+Project 77, Tower of Hanoi: The classic disk-stacking puzzle.
+Use stack data structures to simulate a puzzle state.
+
+The Tower of Hanoi is a stack-moving puzzle game that features three poles on which you can stack
+various-sized disks. The object of the game is to move one tower of disks to another pole. However,
+only one disk can be moved at a time, and larger disks cannot be placed on top of smaller ones.
+Figuring out a certain pattern will help you solve this puzzle. Can you discover it? (Hint: try
+setting the TOTAL_DISKS variable to 3 or 4 to solve an easier version first.)
+"""
