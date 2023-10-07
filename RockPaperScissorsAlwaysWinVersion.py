66770c00c291bae54b3acdb80f768edab76d835d
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-07 09:02:59 -0400

    Create RockPaperScissorsAlwaysWinVersion.py

---
+++ RockPaperScissorsAlwaysWinVersion.py
@@ -0,0 +1,11 @@
+"""
+Project 60, Rock Paper Scissors (Always-Win Version): A version of the game where the player
+cannot lose.
+Create the illusion of randomness in a program.
+
+This variant of Rock Paper Scissors is identical to Project 59, "Rock Paper Scissors",
+except the player will always win. The code selecting the computer's move is set so that
+it always chooses the losing move. You can offer this game to your friends, who may be excited
+when they win... at first. See how long it takes before they catch on to the fact that the
+game is rigged in their favor.
+"""
