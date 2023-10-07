32b2d9ab2350aa5de942bc1d045a8f50f1b5a93f
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-07 10:22:12 -0400

    Create SlidingTilePuzzle.py

---
+++ SlidingTilePuzzle.py
@@ -0,0 +1,12 @@
+"""
+Project 68, Sliding Tile Puzzle: The classic four-by-four tile puzzle.
+Employ a data structure to reflect the state of a game board.
+
+This classic puzzle relies on a 4 x 4 board with 15 numbered tiles and one free space.
+The objective is to slide the tiles until the numbers are in the correct order, going left
+to right and top to bottom. Tiles can only slide; you're not allowed to directly pick them
+up and rearrange them. Some versions of this puzzle toy feature scrambled images that form
+a complete picture once solved.
+More information about sliding tile puzzles can be found at
+https://en.wikipedia.org/wiki/Sliding_puzzle
+"""
