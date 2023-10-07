3f751fd90e802fdbb51405074fccda14cd392420
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-07 11:37:12 -0400

    Create TwentyFortyEight.py

---
+++ TwentyFortyEight.py
@@ -0,0 +1,10 @@
+"""
+Project 79, Twenty Forty-Eight: A casual tile matching game.
+Simulate gravity to make tiles "fall" in arbitrary directions.
+
+Gabriele Cirulli, a web developer, invented the game 2048 in one weekend. It was inspired by Veewo
+Studios' 1024 game, which in turn was inspired by Threes!, a game by the development team Sirvo.
+In 2048, you must merge numbers on a 4x4 board to clear them from the screen. Two 2s merge into a 4,
+two 4s merge into an 8, and so on. The game adds a new 2 to the board on each merging. The objective
+is to reach 2048 before the entire board fills up.
+"""
