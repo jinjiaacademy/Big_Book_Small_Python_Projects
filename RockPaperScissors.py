40f9470620ebc3ea2c9de28da93067bc0e6ba8fb
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-07 09:02:53 -0400

    Create RockPaperScissors.py

---
+++ RockPaperScissors.py
@@ -0,0 +1,10 @@
+"""
+Project 59, Rock Paper Scissors: The classic hand game for two players.
+Implement basic game rules as a program.
+
+In this version of the two-player hand game also known as Rochambeau or jan-ken-pon,
+the player faces off against the computer. You can pick either rock, paper, or scissors.
+Rock beats scissors, scissors beats paper, and paper beats rock. This program adds some
+brief pauses for suspense.
+For a variation of this game, see Project 60, "Rock Paper Scissors (Always-Win Version)."
+"""
