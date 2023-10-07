048c98460c49c88673d184eb054e80f31cee2991
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-07 11:45:56 -0400

    Create WaterBucket.py

---
+++ WaterBucket.py
@@ -0,0 +1,13 @@
+"""
+Project 81, Water Bucket Puzzle: Obtain exactly four liters of water by filling and emptying
+three buckets.
+Use string templates to generate ASCII art.
+
+In this solitaire puzzle game, you must use three buckets (three-liter, five-liter, and
+eight-liter buckets) to collect exactly four liters of water in one of the buckets. Buckets
+can only be emptied, completely filled, or poured into another bucket. For example, you can fill
+the five-liter bucket and then pour its contents into the three-liter bucket, leaving you with
+a full three-liter bucket and two liters of water in the five-liter bucket. With some effort,
+you should be able to solve the puzzle. But can you figure out how to solve it with the minimal
+number of moves?
+"""
