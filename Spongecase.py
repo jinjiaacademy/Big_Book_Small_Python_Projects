7f10f26a68d970bc807264ae7d798338443fa984
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-07 11:10:29 -0400

    Create Spongecase.py

---
+++ Spongecase.py
@@ -0,0 +1,12 @@
+"""
+Project 72, sPoNgEcAsE: Translate English messages into sPOnGEcAsE.
+Change the casing of letters in strings.
+
+You've probably seen the "Mocking SpongeBob" meme: a picture of SpongeBob SquarePants, with a caption
+whose text alternates between upper- and lowercase letters to indicate sarcasm, like this:
+uSiNg SpOnGeBoB MeMeS dOeS NoT mAkE YoU wItTy. For some randomness, the text sometimes doesn't alternate
+capitalization.
+This short program uses the upper() and lower() string methods to convert your message into "spongecase."
+The program is also set up so that other programs can import it as a module with import spongecase and then
+call the spongecase.englishToSpongecase() function.
+"""
