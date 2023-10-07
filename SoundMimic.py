01a88ad9339eb4c17514265c57d0a930fb380523
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-07 11:10:24 -0400

    Create SoundMimic.py

---
+++ SoundMimic.py
@@ -0,0 +1,18 @@
+"""
+Project 71, Sound Mimic: Memorize an increasingly long pattern of sounds.
+Play sound files from a Python program.
+
+Similar to the Simon electronic toy, this memorization game uses the third-party playsound
+module to play four different sounds, which correspond to the A, S, D, and F keys on the keyboard.
+As you successfully repeat the pattern the game gives you, the patterns get longer and longer.
+How many sounds can you hold in your short-term memory?
+If you look at the code, you'll see that the playsound.playsound() function is passed the filename
+of the sound to play. You can download the sound files from these URLs:
+    https://inventwithpython.com/soundA.wav
+    https://inventwithpython.com/soundS.wav
+    https://inventwithpython.com/soundD.wav
+    https://inventwithpython.com/soundF.wav
+Place these files in the same folder as soundmimic.py before running the program. More information
+about the playsound module can be found at https://pypi.org/project/playsound/. Users on macOS must
+also install the pyobjc module from https://pypi.org/project/pyobjc/ for playsound to work.
+"""
