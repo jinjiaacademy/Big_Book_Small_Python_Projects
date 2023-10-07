cb35e9fd318dfeffb32f8453cae405669677f47d
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-07 10:07:07 -0400

    Create SimpleSubstitutionCipher.py

---
+++ SimpleSubstitutionCipher.py
@@ -0,0 +1,11 @@
+"""
+Project 66, Simple Substitution Cipher: An encryption scheme more advanced than the Caesar cipher.
+Perform intermediate math on text.
+
+The Simple Substitution Cipher substitutes one letter for another. Since there are 26 possible
+substitutions for the letter A, 25 possible substitutions for B, 24 for C, and so on, the total
+number of possible keys is 26x25x24x23x...x1, or 403,291,461,126,605,635,584,000,000 keys! That's
+far too many keys for even a supercomputer to brute force, so the code-breaking method used in
+Project 7, "Caesar Hacker" can't be used against the simple cipher. Unfortunately, devious attackers
+can take advantage of known weakness to break the code.
+"""
