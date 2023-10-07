dd030d00bf5d3c96eaaa1acc973d0eb141a7ef8c
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-07 11:15:18 -0400

    Create ThreeCardMonte.py

---
+++ ThreeCardMonte.py
@@ -0,0 +1,16 @@
+"""
+Project 75, Three-Card Monte: The tricky fast-swapping card game that scammers play on tourists.
+Manipulate a data structure based on random movements.
+
+Three-card monte is a common scam played on gullible tourists and other easy marks.
+Three playing cards, one of which is the “red lady” Queen of Hearts, are put facedown on a
+cardboard box. The dealer quickly rearranges the cards and then asks the mark to pick the
+Queen of Hearts. But the dealer can use all sorts of tricks to hide the card or otherwise cheat,
+guaranteeing that the victim never wins. It’s also common for the dealer to have shills in the
+crowd who secretly work with the dealer but pretend to win the game (to make the victim think they
+too could win) or purposefully lose badly (to make the victim think they could do much better).
+This program shows the three cards and then quickly describes a series of swaps. At the end,
+it clears the screen, and the player must pick a card.
+Can you keep up with the “red lady”? For the authentic three-card monte experience, you can enable
+the cheat feature, which causes the player to always lose, even if they select the correct card.
+"""
