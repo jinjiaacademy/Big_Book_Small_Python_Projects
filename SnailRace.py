ab79cdcf84be6567033f322c0a5f820bdaac183b
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-07 10:41:51 -0400

    Create SnailRace.py

---
+++ SnailRace.py
@@ -0,0 +1,10 @@
+"""
+Project 69, Snail Race: Fast-paced snail racing action!
+Calculate spacing for ASCII art snails.
+
+You won't be able to handle the fast-paced excitement of these racing ... snails.
+But what they lack in speed they make up for in ASCII-art cuteness. Each snail (represtned by
+an @ character for the shell and v for the two eyestalks) moves slowly but surely toward the
+finish line. Up to eight snails, each with a custom name, can race each other, leaving a slime
+trail in their wake. This program is good for beginners.
+"""
