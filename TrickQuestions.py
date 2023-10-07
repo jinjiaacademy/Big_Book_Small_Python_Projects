8f9d8a844f091253612f59380b6820b40854cc65
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-07 11:37:08 -0400

    Create TrickQuestions.py

---
+++ TrickQuestions.py
@@ -0,0 +1,12 @@
+"""
+Project 78, Trick Questions: A quiz of simple questions with misleading answers.
+Parse the user's text to recognize keywords.
+
+What does a yellow stone thrown into a blue pond become? Do they have a 4th of July in England?
+How can a doctor go 30 days without sleep? Whatever you think the answers to these trick questions
+are, you're probably wrong. The 54 questions in this program have been specifically crafted so that
+their answers are simple, obvious, and misleading. Finding the true answer will require some cleverness.
+Copying the code from this book will spoil the fun, since you'll see the answers, so you might want
+to download and play this game from https://inventwithpython.com/trickquestions.py before looking at
+the source code.
+"""
