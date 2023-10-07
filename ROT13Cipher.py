f694733bccb2cfd1e86db8543eef6ba6bb51f1ca
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-07 09:20:07 -0400

    Create ROT13Cipher.py

---
+++ ROT13Cipher.py
@@ -0,0 +1,12 @@
+"""
+Project 61, ROT13 Cipher: The simplest cipher for encrypting and decrypting text.
+Convert between letters and numbers to perform math on text.
+
+The ROT13 cipher, one of the simplest encryption algorithms, stands for "rotate 13 spaces".
+The cypher represents the letters A to Z as the numbers 0 to 25 in such a way that the encrypted
+letter is 13 spaces from the plain-text letter: A becomes N, B becomes O, and so on. The encryption
+process is identical to the decryption process, making it trivial to program. However, the encryption
+is also trivial to break. Because of this, you'll most often find ROT13 used to conceal non-sensitive
+information, such as spoilers or trivia answers, so it's not read unintentionally. More information
+about the ROT13 cipher can be found at https://en.wikipedia.org/wiki/ROT13.
+"""
