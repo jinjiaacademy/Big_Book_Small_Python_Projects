b80973432dfd3d157f5273196b5288227caeb6b4
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-07 11:10:40 -0400

    Create TextToSpeechTalker.py

---
+++ TextToSpeechTalker.py
@@ -0,0 +1,11 @@
+"""
+Project 74, Text-to-Speech Talker: Make your computer talk to you!
+Use your operating system's text-to-speech engine.
+
+This program demonstrates the use of the pyttsx3 third-party module. Any message you enter will
+be spoken out loud by your operating system's text-to-speech capabilities. Although computer-generated
+speech is an incredibly complex branch of computer science, the pyttsx3 module provides an easy
+interface for it, making this small program suitable for beginners. Once you've learned how to use
+the module, you can add generated speech to your own programs.
+More information about the pyttsx3 module can be found at https://pypi.org/project/pyttsx3/
+"""
