8fd62eaf8a72df5ed73abf8ad0cf9f75b88fbc84
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-07 09:20:12 -0400

    Create RotatingCube.py

---
+++ RotatingCube.py
@@ -0,0 +1,12 @@
+"""
+Project 62, Rotating Cube: A rotating cube animation.
+Learn 3D rotation and line drawing algorithms.
+
+This project features an animation of a 3D cube rotating using trigonometric functions.
+You can adapt the 3D point rotation math and the line() function in your own animation
+programs.
+Although the block text characters we'll use to draw the cube don't look like thin, straight
+lines, this kind of drawing is called a wireframe model because it renders only the edges of
+an object's surfaces. Figure 62-1 shows the wireframe model for a cube and an icosphere, a
+rough sphere made of triangles.
+"""
