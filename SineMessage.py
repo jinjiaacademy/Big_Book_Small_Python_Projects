82fec1c0648b2fdc81b5ec0de8eeeb2bb7ed1446
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-07 10:15:12 -0400

    Create SineMessage.py

---
+++ SineMessage.py
@@ -0,0 +1,8 @@
+"""
+Project 67, Sine Message: Display a scrolling wave message.
+Use a trigonometry function for animation.
+
+This program displays a message of the user's choice in a wavy pattern as the text scrolls up.
+It accomplishes this effect with math.sin(), which implements the trigonometric sine wave function.
+But even if you don't understand the math, this program is rather short and easy to copy.
+"""
