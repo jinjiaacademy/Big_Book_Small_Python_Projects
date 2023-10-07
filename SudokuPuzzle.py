93926102a056644c70bc4ad9f909911e9570a700
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-07 11:10:34 -0400

    Create SudokuPuzzle.py

---
+++ SudokuPuzzle.py
@@ -0,0 +1,9 @@
+"""
+Project 73, Sudoku Puzzle: The classic nine-by-nine newspaper deduction puzzle.
+Model a puzzle with a data structure.
+
+Sudoku is a popular puzzle game in newspapers and mobile apps. The Sudoku board is a 9x9 grid
+in which the player must place the digits 1 to 9 once, and only once, in each row, column, and
+3x3 subgrid. The game begins with a few spaces already filled in with digits, called givens. A
+well-formed Sudoku puzzle will have only one possible valid solution.
+"""
