20d25f15932713513f605c4397371bab6cf3c074
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-10-08 09:19:45 -0400

    Update CaesarCipher.py

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -2,7 +2,7 @@
 Project 6: Caesar Cipher: A simple encryption scheme used thousands of years ago.
 Convert between letters and numbers to perform math on text.
 
-The Caesar cipher is an incient encryption algorithm used by Julius Caesar. It encrypts letters by
+The Caesar cipher is an ancient encryption algorithm used by Julius Caesar. It encrypts letters by
 shifting them over by a certain number of places in the alphabet. We call the length of shift the
 key. For example, if the key is 3, then A becomes D, B becomes E, C becomes F, and so on. To decrypt
 the message, you must shift the encrypted letters in the opposite direction. This program lets the
